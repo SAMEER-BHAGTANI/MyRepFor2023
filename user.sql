2c45f289b3e95a9cb949bdb8e47a570f37889137
SAMEER-BHAGTANI <114795359+SAMEER-BHAGTANI@users.noreply.github.com>
2023-10-08 16:44:30 +0530

    Create user.sql

---
+++ user.sql
@@ -0,0 +1,11 @@
+create database sam;
+
+use sam;
+
+create table users(u_id int(10) PRIMARY KEY, username char(25), password char(50));
+
+insert into users (1,"Raman","Hello@123");
+insert into users (2,"Anup","123456");
+insert into users (3,"Chetan","Chetan@456");
+insert into users (4,"Dinesh","Hi@101");
+insert into users (5,"Kapil","KapilisKing@100");
