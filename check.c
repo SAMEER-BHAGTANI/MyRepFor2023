cd87f0623099833e2df93e0ab9ea4e067256bfbd
SAMEER-BHAGTANI <114795359+SAMEER-BHAGTANI@users.noreply.github.com>
2023-10-09 16:18:45 +0530

    Create check.c

---
+++ check.c
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+  int a;
+
+  clrscr();
+
+  cout<<"\n\t\t\tEnter any number : ";
+
+  if(a/2=0)
+  {
+    cout<<"\n\t\t\tEven number";
+  }
+  else
+  {
+    cout<<"\n\t\t\tOdd number";
+  }
+
+  getch();
+}
