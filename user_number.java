e7eb227c5c35de64338df6c7ccbec9999c6f7c90
SAMEER-BHAGTANI <114795359+SAMEER-BHAGTANI@users.noreply.github.com>
2023-10-09 21:47:40 -0700

    Create user_number.java

---
+++ user_number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class hello
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Enter any number : ");
+    a = x.nextInt();
+
+    System.out.println("\n\t\t\tYou have entered : " + a);
+  }
+}
