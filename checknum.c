dce64026d0196fe5874ece5816380dc8fdf3fbe1
SAMEER-BHAGTANI <114795359+SAMEER-BHAGTANI@users.noreply.github.com>
2023-10-10 14:44:22 +0530

    Create checknum.c

---
+++ checknum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int a;
+
+  clrscr();
+
+  printf("\n\t\t\tEnter any number : ");
+
+  if(a>0)
+  {
+    printf("\n\t\t\tPositive Number");
+  }
+  else if(a<0)
+  {
+    printf("\n\t\t\tNegative number");
+  }
+}
